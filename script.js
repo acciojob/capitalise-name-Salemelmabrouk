701ce0413c97938616b1b109182c026617907d52
Salemelmabrouk <149633706+Salemelmabrouk@users.noreply.github.com>
2024-02-28 20:33:05 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var input = document.getElementById("input");
+    input.addEventListener("blur", function() {
+        this.value = this.value.toUpperCase();
+    });
+});
